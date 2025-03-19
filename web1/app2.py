890c1c6b6dac9f909f2dcd18c5a23792e99b24ce
Julian Kanjere <julian.kanjere@jskdezign.com>
2025-03-19 15:31:37 +0200

    added basic Flask example with HTML templates

---
+++ web1/app2.py
@@ -0,0 +1,18 @@
+# usage python app2.py
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def student():
+    return render_template('student.html')
+
+@app.route('/result', methods=['POST', 'GET'])
+def result():
+    if request.method == 'POST':
+        result = request.form
+        return render_template("result.html", result=result)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=8008, debug=True)
